cbe445f22b239433a07b132c4ef31c833148c09f
Cédric Jäger <52103663+CedricJaeger@users.noreply.github.com>
2026-01-20 22:31:09 +0100

    Added an utility class, exceptions and javadoc

---
+++ src/main/java/link/jaeger/steuerabzug3a/Steuerabzug3a.java
@@ -0,0 +1,59 @@
+package link.jaeger.steuerabzug3a;
+
+import link.jaeger.steuerabzug3a.exceptions.InvalidMinimalYearException;
+import link.jaeger.steuerabzug3a.exceptions.UnsupportedMaximalYearException;
+
+/**
+ * Utility class to get the maximal deductible amount for a given fiscal year
+ *
+ * @version 2026.1.0
+ * @author Cédric Jäger
+ */
+public final class Steuerabzug3a {
+
+    /** The minimal fiscal year which is supported. */
+    static final int MIN_SUPPORTED_YEAR = 1985;
+    /** The maximal fiscal year which is supported. */
+    static final int MAX_SUPPORTED_YEAR = 2026;
+
+    private Steuerabzug3a() {}
+
+    /**
+     * Return the maximal deductible amount for a given fiscal year for a person with an affiliation to a second
+     * pillar institution.
+     *
+     * @param jahr the fiscal year.
+     * @return the maximal deductible amount in CHF.
+     * @throws InvalidMinimalYearException is thrown when the year is before {@link Steuerabzug3a#MIN_SUPPORTED_YEAR}.
+     * @throws UnsupportedMaximalYearException is thrown when the year is after {@link Steuerabzug3a#MAX_SUPPORTED_YEAR}.
+     *
+     */
+    public static int mitZweiteSaeule(int jahr) {
+        if (jahr < MIN_SUPPORTED_YEAR) {
+            throw new InvalidMinimalYearException("The year provided is invalid! Year provided = '" + jahr + "'!");
+        } else if (jahr > MAX_SUPPORTED_YEAR) {
+            throw new UnsupportedMaximalYearException("The year provided is not yet supported! Year provided = '" + jahr + "'!");
+        }
+        return Steuerabzug3aData.valueOf("JAHR_" + jahr).mitZweiteSaeule();
+    }
+
+    /**
+     * Return the maximal deductible amount for a given fiscal year for a person without an affiliation to a second
+     * pillar institution.
+     *
+     * @param jahr the fiscal year.
+     * @return the maximal deductible amount in CHF.
+     * @throws InvalidMinimalYearException is thrown when the year is before {@link Steuerabzug3a#MIN_SUPPORTED_YEAR}.
+     * @throws UnsupportedMaximalYearException is thrown when the year is after {@link Steuerabzug3a#MAX_SUPPORTED_YEAR}.
+     */
+    public static int ohneZweiteSaeule(int jahr)
+    {
+        if (jahr < MIN_SUPPORTED_YEAR) {
+            throw new InvalidMinimalYearException("The year provided is invalid! Year provided = '" + jahr + "'!");
+        } else if (jahr > MAX_SUPPORTED_YEAR) {
+            throw new UnsupportedMaximalYearException("The year provided is not yet supported! Year provided = '" + jahr + "'!");
+        }
+        return Steuerabzug3aData.valueOf("JAHR_" + jahr).ohneZweiteSaeule();
+    }
+
+}
---
+++ src/test/java/link/jaeger/steuerabzug3a/exceptions/InvalidMinimalYearExceptionTest.java
@@ -0,0 +1,44 @@
+package link.jaeger.steuerabzug3a.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InvalidMinimalYearExceptionTest {
+
+    @Test
+    public void testInvalidMinimalYearExceptionDefaultConstructorTest() {
+        String expectedMessage = "The minimal year provided is invalid!";
+        Exception exception = new InvalidMinimalYearException();
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void testInvalidMinimalYearExceptionConstructorWithMessageParameterTest() {
+        String expectedMessage = "A test message!";
+        Exception exception = new InvalidMinimalYearException(expectedMessage);
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void testInvalidMinimalYearExceptionConstructorWithMessageAndCauseParametersTest() {
+        String expectedMessage = "A test message!";
+        Throwable expectedCause = new Throwable();
+        Exception exception = new InvalidMinimalYearException(expectedMessage, expectedCause);
+        String actualMessage = exception.getMessage();
+        Throwable actualCause = exception.getCause();
+        assertEquals(expectedMessage, actualMessage);
+        assertEquals(expectedCause, actualCause);
+    }
+
+    @Test
+    public void testInvalidMinimalYearExceptionConstructorWithCauseParameterTest() {
+        Throwable expectedCause = new Throwable();
+        Exception exception = new InvalidMinimalYearException(expectedCause);
+        Throwable actualCause = exception.getCause();
+        assertEquals(expectedCause, actualCause);
+    }
+
+}
